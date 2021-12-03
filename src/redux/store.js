450e3091e7bcb9a93b5a667b61b82a452ebd7fe7
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-12-03 15:19:42 +0000

    store actions and reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import weatherReducer from './slices/weatherSlices'
+
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    reducer: weatherReducer,
   },
 });
